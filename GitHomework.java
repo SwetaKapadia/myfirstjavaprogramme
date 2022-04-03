58b7ee7bb8bada52a9bf7a3ebe0dc7dbd1fec443
swetakapadia <sweta.srshah@gmail.com>
2022-04-03 19:27:15 +0100

    git homework added

---
+++ GitHomework.java
@@ -0,0 +1,10 @@
+package com.learning.java;
+
+public class GitHomework {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("My first Git homework");
+    }
+}
--- MyFirstJavaProgram.java
+++ MyFirstJavaProgram.java
@@ -5,6 +5,6 @@ public class MyFirstJavaProgram {
     public static void main(String[] args) {
 
         System.out.println("Sweta Kapadia");
-        System.out.println("hello world")
+        System.out.println("hello world");
     }
 }
